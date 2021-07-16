2fe7d99ab44e43c99ef6cfa3665338058755a87b
khanhdpdx01 <tankhanh2k1@gmail.com>
2021-07-16 17:17:31 +0700

    add user entity

---
+++ src/main/java/com/khanhdpdx/shorten_url/entity/User.java
@@ -0,0 +1,11 @@
+package com.khanhdpdx.shorten_url.entity;
+
+import java.util.Date;
+
+public class User {
+    private Long userId;
+    private String name;
+    private String email;
+    private Date creationDate;
+    private Date lastLogin;
+}
